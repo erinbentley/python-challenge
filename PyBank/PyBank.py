8606f836a315af9ca6b09d33cdac048c0fdc4946
E Bentley <ebentley@Es-MacBook-Air.local>
2019-11-23 16:22:38 -0600

    Commit changes with total months working

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,13 +1,21 @@
+#Erin Bentley
+#Data Viz Boot Camp
+#Python Homework
 import os
 import csv
 
-csvpath = os.path.join("python-challenge", "budget_data.csv")
+csvpath = os.path.join("../PyBank","budget_data.csv")
+#The total number of months included in the dataset
+total_months = 0
 
 
+#open budget csv file
+with open(csvpath,"r") as Financial_Details:
+    csvreader = csv.DictReader(Financial_Details)
+    for row in csvreader:
+        total_months = total_months + 1
+#print(total_months)
 
 
-
-
-
-output_file = os.path.join("Financial Details")
-
+#output_file = os.path.join("Financial Details")
+    
