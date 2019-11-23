8507e1f991a35885771bd9449151f3e022ab2a03
E Bentley <ebentley@Es-MacBook-Air.local>
2019-11-23 16:43:08 -0600

    Add Net Total Profit/Losses

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -7,6 +7,8 @@ import csv
 csvpath = os.path.join("../PyBank","budget_data.csv")
 #The total number of months included in the dataset
 total_months = 0
+# The net total amount of "Profit/Losses" over the entire period
+total_profit = 0
 
 
 #open budget csv file
@@ -14,7 +16,12 @@ with open(csvpath,"r") as Financial_Details:
     csvreader = csv.DictReader(Financial_Details)
     for row in csvreader:
         total_months = total_months + 1
+        #tells the code to count the rows and keep a tally
+        total_profit = total_profit + int(row["Profit/Losses"])
+        #tells the code to calculate 0 + the row (converted into an integer)
+
 #print(total_months)
+#print(total_profit)
 
 
 #output_file = os.path.join("Financial Details")
