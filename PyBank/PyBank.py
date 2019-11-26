59289905eed0ad793fa49a4d33ae3028299e91b2
E Bentley <ebentley@Es-MacBook-Air.local>
2019-11-26 11:01:09 -0600

    Add min and max with prints.

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -16,7 +16,7 @@ previous_revenue = 0
 # We have to define revenue_change_list as a list so that the output can be added up and an average can be found
 # [] in Python means that all these values are going to be a list. 
 revenue_change_list = []
-
+change_dates = []
 
 #open budget csv file
 with open(csvpath,"r") as Financial_Details:
@@ -34,19 +34,46 @@ with open(csvpath,"r") as Financial_Details:
         #print(total_profit)
 
         revenue_diff = float(row[1]) - previous_revenue
+        #print(row[0])
+        dates = row [0]
         previous_revenue = float(row[1])
         revenue_change_list.append(revenue_diff)
-    #print(revenue_change_list)
-    average_revenue = round(sum(revenue_change_list[1:])/len(revenue_change_list[1:]), 2)
-    print(average_revenue)
-        #update the variable "previous_revenue"; previous_revenue = float(row[1]) to show that now previous revenue = previous month
+        change_dates.append(dates)
 
-    #print(revenue_diff)
+    #print(len(revenue_change_list))
+    #print(len(change_dates))
+ 
+    average_revenue = round(sum(revenue_change_list[1:])/len(revenue_change_list[1:]), 2)
+    #print(average_revenue)
+        #update the variable "previous_revenue"; previous_revenue = float(row[1]) to show that now previous revenue = previous month
         
 
+max_change = max(revenue_change_list)
+max_change_index = revenue_change_list.index(max_change)
+max_date = change_dates [max_change_index]
 
+#max is the 25th postition from the revenue change list 
 
+#print(max_change_index)
+#print(max_date)
+#print(revenue_diff)
+#print (max_change)
 
+min_change = min(revenue_change_list)
+min_change_index = revenue_change_list.index(min_change)
+min_date = change_dates [min_change_index]
+        
+#print (min_change_index)
+#print (min_date)
+#min is the 44th position (or index) 
+#print (min_change)
 
-#output_file = os.path.join("Financial Details")
+output_file = os.path.join("Financial Details")
+print("Financial_Details")
+print("________")
+print("Total Months: " + str(total_months))
+print("Average Revenue: " + str(average_revenue))
+print("Greatest Increase in Profits: " + str(max_date) +": " + str(max_change))
+print ("Greatest Decrease in Profits: " + str(min_date) +": "+ str(min_change))
+print("________")
     
